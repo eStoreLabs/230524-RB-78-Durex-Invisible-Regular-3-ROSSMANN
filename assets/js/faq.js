1871f1af6c085c2fccebeb2804e8c71ba6aad40a
Dominika Wojewska <130984276+Domi-eStore@users.noreply.github.com>
2023-10-10 13:01:41 +0200

    add slider

--- assets/js/faq.js
+++ assets/js/faq.js
@@ -3,11 +3,12 @@ const esArrow = document.querySelectorAll(".es-faq__arrow");
 esQuestion.forEach((question) => {
   question.addEventListener("click", toggleAnswer);
 });
-function toggleAnswer(e) {
-  const esArrow = e.target.closest(".es-faq__single").children[0].children[1];
-  esArrow.classList.toggle("es-faq__arrow--open");
-  const closestElement = e.target.closest(".es-faq__single").children[0];
-  closestElement.parentElement.children[1].classList.toggle(
-    "es-faq__content--open"
-  );
+
+function toggleAnswer(event) {
+  const faqSingle = event.target.closest(".es-faq__single");
+  const faqArrow = faqSingle.querySelector(".es-faq__arrow");
+  const faqContent = faqSingle.querySelector(".es-faq__content");
+
+  faqArrow.classList.toggle("es-faq__arrow--open");
+  faqContent.classList.toggle("es-faq__content--open");
 }
